d7ec0cc6fccf5f22787141cad5b3e2ea50263075
dduran1967 <dave@yodata.io>
2022-10-27 13:24:12 -0700

    replay api works

--- src/pages/api/replay/index.js
+++ src/pages/api/replay/index.js
@@ -1,3 +1,4 @@
+const logger = require('@yodata/logger')
 
 /**
  * calls the replay api and returnsthe response
@@ -9,9 +10,24 @@ export default async function handler(request, response) {
   const { default: got } = await import('got')
   const staging = 'https://gxlejs63x9.execute-api.us-west-2.amazonaws.com/replay'
   const test = 'http://example.com/replay'
-  const target = process.env.REPLAY_TARGET || staging || test
-  return got(target, request.body)
-    .then(res => {
-      return response.status(res.statusCode).json(res.body)
+  const target = process.env.API_REPLAY_TARGET || staging || test
+  return got.post(target, {
+    json: request.body
+  }).then(res => {
+    const {data, statusCode, statusMessage, body, json} = res
+    // logger.json({data, statusCode, statusMessage, body, json})
+    response.status(res.statusCode).json(JSON.parse(res.body))
+  })
+    .catch(error => {
+      logger.error(error.message, error.stack)
+      if (error.res) {
+        logger.error(error)
+        response.status(500).json({
+          actionStatus: 'FailedActionStatus',
+          error: error.message
+        })
+      }
+
     })
+
 }

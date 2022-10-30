f1a672f2fc7e699386d3d3e954222afbb8e2f269
dduran1967 <dave@yodata.io>
2022-10-30 14:56:18 -0700

    json-schema-object generator works

--- generator/data/helpers/merge-schema-items.js
+++ generator/data/helpers/merge-schema-items.js
@@ -1,9 +1,39 @@
+/** @format */
+
 // import Schema from '@asyncapi/parser/lib/models/schema'
 import { merge } from 'merge-anything'
+import { Schema } from '@asyncapi/parser/lib/models/schema'
 
-export function mergeSchemaItems (schema = {}) {
+const observeOverwrites = (currentValue, newValue, key) => {
+  console.log('OVERWRITING', {
+    key, currentValue, newValue
+  })
+  return newValue
+}
+
+/**
+ * @description merges json schema "allOf" into a single schema
+ * @param {} source
+ * @returns {object<JSONSchema>}
+ */
+export function mergeAllOf(source /*: Schema */) /*: Schema */ {
+  let result = source?._json ? { ...source._json } : { ...source }
+  if (Array.isArray(result.allOf)) {
+    console.log('ORIGINAL_ALL_OF', result.allOf)
+    result = merge(observeOverwrites, ...result.allOf, result)
+    console.log('NEW_ALL_OF', result.allOf)
+    delete result.allOf
+  }
+  return result
+}
+
+export function mergeSchemaItems(source) {
   let result = schema
-  if (schema && typeof schema?.allOf === 'function' && Array.isArray(schema.allOf())) {
+  if (
+    schema &&
+    typeof schema?.allOf === 'function' &&
+    Array.isArray(schema.allOf())
+  ) {
     result = merge([schema, ...schema.allOf()])
   }
   return result
